9f9ecb4acee8db9103663e7c86250b710fd0cacd
Lydia <Lydia@DESKTOP-LUMGOJD>
2016-11-26 22:16:30 +0000

    Add project files.

---
+++ TextWizard/TextWizard.cpp
@@ -0,0 +1,13 @@
+// TextWizard.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "hello" << endl;
+    return 0;
+}
+
