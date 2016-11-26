67dc2af377d3ef10429f3c3d4ae418adfad9692c
LydiaAndriopoulou <andriopouloulydia@gmail.com>
2016-11-26 22:21:04 +0000

    Create remove_extra_space function

--- TextWizard/TextWizard.cpp
+++ TextWizard/TextWizard.cpp
@@ -1,13 +1,31 @@
-// TextWizard.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-int main(){
-	cout << "hello" << endl;
-    return 0;
+const char SPACE = ' ';
+
+string remove_extra_spaces(string document) {
+	string new_document;
+	for (int i = 0; i < document.size(); i++) {
+		char character = document[i];
+		if (character == SPACE) {
+			new_document.push_back(character);
+			while (character == SPACE) {
+				i++;
+				character = document[i];
+			}
+		}
+		new_document.push_back(character);
+	}
+	return new_document;
 }
 
+
+int main() {
+	string document;
+	getline(cin, document);
+	cout << remove_extra_spaces(document);
+}
