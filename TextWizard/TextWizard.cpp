c099e82dcd15b11578732502c82ed2fa01dcc4ac
LydiaAndriopoulou <andriopouloulydia@gmail.com>
2016-11-27 20:01:24 +0000

    Add capitilise_words_after_stopping_points

--- TextWizard/TextWizard.cpp
+++ TextWizard/TextWizard.cpp
@@ -2,10 +2,32 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <ctype.h>
 
 using namespace std;
 
 const char SPACE = ' ';
+const char DOT = '.';
+const char BANG = '!';
+
+string capitilise_words_after_stopping_points(string document) {
+	string new_document;
+	for (int i = 0; i < document.size(); i++) {
+		char character = document[i];
+		new_document.push_back(character);
+		if (character == DOT || character == BANG) {
+			i++;
+			character = document[i];
+			if (character == SPACE) {
+				new_document.push_back(character);
+				i++;
+				character = document[i];
+				new_document.push_back(toupper(character));
+			}
+		}
+	}
+	return new_document;
+}
 
 string remove_extra_spaces(string document) {
 	string new_document;
@@ -27,5 +49,7 @@ string remove_extra_spaces(string document) {
 int main() {
 	string document;
 	getline(cin, document);
-	cout << remove_extra_spaces(document);
+	document = remove_extra_spaces(document);
+	document = capitilise_words_after_stopping_points(document);
+	cout << document << endl;
 }
